fd4adcd5ab4f72882726f137410b37c17ca8cc92
Viveksapkale22 <viveksapkale0022@gmail.com>
2026-08-16 17:41:35 +0530

    back-end conection up2

--- backend/socket/socketHandler.js
+++ backend/socket/socketHandler.js
@@ -2,13 +2,12 @@ const { Server } = require('socket.io');
 
 let io;
 
-function initSocket(server) {
-  io = new Server(server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST'],
-    },
-  });
+const io = new Server(server, {
+  cors: {
+    origin: ['http://localhost:5173', 'https://procter-exam-system.vercel.app'],
+    methods: ['GET', 'POST']
+  }
+});
 
   io.on('connection', (socket) => {
     console.log('Socket connected:', socket.id);
